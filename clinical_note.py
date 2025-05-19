0056fed0b82ab144dc2dc5b33bc6abb7b7a42309
zaidatom362 <moham362@uwm.edu>
2025-05-18 22:51:51 -0500

    Delete clinical_note.py

--- clinical_note.py
+++
@@ -1,8 +0,0 @@
-"""
-clinical_note.py - Single note attached to a visit.
-"""
-class ClinicalNote:
-    def __init__(self, nid, vid, text):
-        self.id       = nid
-        self.visit_id = vid
-        self.text     = text
