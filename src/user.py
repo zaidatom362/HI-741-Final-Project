5d766ac08553675b656f1e5dd47ae691645c7acb
zaidatom362 <moham362@uwm.edu>
2025-05-19 23:12:34 -0500

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,7 +1,4 @@
-
-# Represents a user and their allowed actions
 # user.py
-
 class User:
     """A user with a role and permission checking."""
     def __init__(self, username, role):
@@ -10,7 +7,7 @@ class User:
 
     def can_perform(self, action):
         permissions = {
-            'admin': ['count_visits', 'generate_statistics'],
+            'admin': ['count_visits'],
             'clinician': ['add_patient', 'remove_patient', 'retrieve_patient', 'view_note', 'count_visits'],
             'nurse': ['add_patient', 'remove_patient', 'retrieve_patient', 'view_note', 'count_visits'],
             'management': ['generate_statistics']
