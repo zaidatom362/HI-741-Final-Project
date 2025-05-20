00757a9329d6ef5b2ee7b85017ca65d0903de2bd
zaidatom362 <moham362@uwm.edu>
2025-05-19 19:26:33 -0500

    Moved to src

--- src/department.py
+++ src/department.py
@@ -1,5 +1,4 @@
 # department.py
-# Represents a hospital department and its patients
 
 class Department:
     """A department contains a set of patients."""
@@ -8,9 +7,7 @@ class Department:
         self.patients = set()
 
     def add_patient(self, patient):
-        """Add a patient to this department."""
         self.patients.add(patient)
 
     def remove_patient(self, patient):
-        """Remove a patient if present."""
         self.patients.discard(patient)
